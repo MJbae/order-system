3a9c948e46ac1d9f214908804f5232d57edfdbda
MJbae <111344711+mj-at-ogq@users.noreply.github.com>
2022-12-15 19:54:48 +0900

    sprint shell 의존성 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,14 +17,22 @@ repositories {
     mavenCentral()
 }
 
+extra["springShellVersion"] = "2.1.4"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.shell:spring-shell-starter")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.shell:spring-shell-dependencies:${property("springShellVersion")}")
+    }
 }
 
 tasks.withType<KotlinCompile> {
