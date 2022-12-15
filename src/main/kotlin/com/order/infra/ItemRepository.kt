9eeecead96b3550f113f112f30c0a43a01ce1a74
MJbae <111344711+mj-at-ogq@users.noreply.github.com>
2022-12-15 21:35:03 +0900

    feat: 동시성 고려하여 낙관적락으로 상품 엠티티 조회하는 기능 추가

--- src/main/kotlin/com/order/infra/ItemRepository.kt
+++ src/main/kotlin/com/order/infra/ItemRepository.kt
@@ -1,6 +1,16 @@
 package com.order.infra
 
 import com.order.domain.Item
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.data.repository.query.Param
+import javax.persistence.LockModeType
 
-interface ItemRepository : JpaRepository<Item, Long>
+interface ItemRepository : JpaRepository<Item, Long> {
+    @Lock(value = LockModeType.OPTIMISTIC)
+    @Query("select i from Item i where i.id = :id")
+    fun findByIdInLock(@Param("id") id: Long) = this.findByIdOrNull(id) ?: throw NotFoundException()
+}
