5e20f1d546b47ab03f5c22ac9b545fa0f133dfad
MJbae <gentlemmj@gmail.com>
2023-01-03 22:56:31 +0900

    refactor: 테스트 준비 구문을 context 이하에 정의

--- src/test/kotlin/com/order/application/OrderUnitTest.kt
+++ src/test/kotlin/com/order/application/OrderUnitTest.kt
@@ -22,51 +22,58 @@ internal class OrderUnitTest : DescribeSpec({
 
     describe("주문 시") {
         context("주문금액이 5만원 이상이라면") {
-            beforeTest {
-                orderQuantity = 2
-                stockQuantity = 7
-                itemPrice = BigDecimal.valueOf(45000)
-            }
-            it("주문금액에 배송료를 포함하지 않는다") {
-                val sut = Customer()
-                val item = Item(itemId, itemPrice, itemName, stockQuantity)
-                val orderData = OrderData(itemId, orderQuantity, item)
-                val expectedPrice = itemPrice.multiply(BigDecimal.valueOf(orderQuantity.toLong()))
+            orderQuantity = 2
+            stockQuantity = 7
+            itemPrice = BigDecimal.valueOf(45000)
 
+            val sut = Customer()
+            val item = Item(itemId, itemPrice, itemName, stockQuantity)
+            val orderData = OrderData(itemId, orderQuantity, item)
+            val expectedPrice = itemPrice.multiply(BigDecimal.valueOf(orderQuantity.toLong()))
+
+            it("주문금액에 배송료를 포함하지 않는다") {
                 val result = sut.order(orderData)
 
                 result.price shouldBe expectedPrice
+            }
+            it("주문이 성공한다") {
+                val result = sut.order(orderData)
+
+                result.isSuccess shouldBe true
             }
         }
         context("주문금액이 5만원 미만이라면") {
-            beforeTest {
-                orderQuantity = 1
-                stockQuantity = 7
-                itemPrice = BigDecimal.valueOf(45000)
-                deliveryFee = BigDecimal.valueOf(2500)
-            }
-            it("주문금액에 배송료를 포함한다") {
-                val sut = Customer()
-                val item = Item(itemId, itemPrice, itemName, stockQuantity)
-                val orderData = OrderData(itemId, orderQuantity, item)
-                val expectedPrice = itemPrice.multiply(BigDecimal.valueOf(orderQuantity.toLong())).add(deliveryFee)
+            orderQuantity = 1
+            stockQuantity = 7
+            itemPrice = BigDecimal.valueOf(45000)
+            deliveryFee = BigDecimal.valueOf(2500)
 
+            val sut = Customer()
+            val item = Item(itemId, itemPrice, itemName, stockQuantity)
+            val orderData = OrderData(itemId, orderQuantity, item)
+            val expectedPrice = itemPrice.multiply(BigDecimal.valueOf(orderQuantity.toLong())).add(deliveryFee)
+
+            it("주문금액에 배송료를 포함한다") {
                 val result = sut.order(orderData)
 
                 result.price shouldBe expectedPrice
             }
+            it("주문이 성공한다") {
+                val result = sut.order(orderData)
+
+                result.isSuccess shouldBe true
+            }
         }
         context("상품의 재고 보다 많은 수량이 주문된다면") {
-            beforeTest {
-                orderQuantity = 10
-                stockQuantity = 7
-                itemPrice = BigDecimal.valueOf(45000)
-            }
-            it("주문이 실패한다") {
-                val sut = Customer()
-                val item = Item(itemId, itemPrice, itemName, stockQuantity)
-                val orderData = OrderData(itemId, orderQuantity, item)
+            orderQuantity = 10
+            stockQuantity = 7
+            itemPrice = BigDecimal.valueOf(45000)
 
+            val sut = Customer()
+            val item = Item(itemId, itemPrice, itemName, stockQuantity)
+            val orderData = OrderData(itemId, orderQuantity, item)
+
+            it("주문이 실패한다") {
                 val result = sut.order(orderData)
 
                 result.isSuccess shouldBe false
