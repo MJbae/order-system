57960183245ce0521373a2beac4f7dd8c42b9c19
MJbae <gentlemmj@gmail.com>
2023-01-17 16:56:28 +0900

    test: 비공개 팩토리 메소드 적용에 따른 준비 구절 간소화

--- src/test/kotlin/com/order/application/OrderIntegrationTest.kt
+++ src/test/kotlin/com/order/application/OrderIntegrationTest.kt
@@ -19,26 +19,21 @@ class OrderIntegrationTest(
 ) : DescribeSpec() {
     override fun extensions() = listOf(SpringExtension)
 
-    private var orderQuantity: Int = 0
-    private var itemPrice: BigDecimal = BigDecimal.valueOf(45000)
-    private var stockQuantity: Int = 7
-    private val itemId: Long = 778422L
     private val order: Order = Order()
 
     init {
         this.describe("주문 시") {
             context("주문금액이 5만원 이상이라면") {
-                orderQuantity = 2
-                stockQuantity = 7
-                itemPrice = BigDecimal.valueOf(45000)
-                val itemName = "캠핑덕 우드롤테이블"
-                val item = Item(itemId, itemPrice, itemName, stockQuantity)
                 it("주문금액에 배송료를 포함하지 않는다") {
+                    val orderData = createOrderData(
+                        itemPrice = BigDecimal.valueOf(45000),
+                        itemName = "캠핑덕 우드롤테이블", stockQuantity = 7, orderQuantity = 2
+                    )
                     sut = OrderService(order, orderRepository, itemRepository)
-                    orderData = OrderData(itemId, orderQuantity, item)
+
                     val result = sut.order(orderData)
 
-                    result.price shouldBe itemPrice.multiply(BigDecimal(orderQuantity))
+                    result.price shouldBe BigDecimal.valueOf(90000)
                     result.isSuccess shouldBe true
                 }
             }
@@ -47,6 +42,16 @@ class OrderIntegrationTest(
 
     companion object {
         private lateinit var sut: OrderService
-        private lateinit var orderData: OrderData
+    }
+
+    val itemId = 778422L
+    private fun createOrderData(
+        itemPrice: BigDecimal,
+        itemName: String,
+        stockQuantity: Int,
+        orderQuantity: Int
+    ): OrderData {
+        val item = Item(this.itemId, itemPrice, itemName, stockQuantity)
+        return OrderData(this.itemId, orderQuantity, item)
     }
 }
--- src/test/kotlin/com/order/application/OrderUnitTest.kt
+++ src/test/kotlin/com/order/application/OrderUnitTest.kt
@@ -12,58 +12,48 @@ import java.math.BigDecimal
 internal class OrderUnitTest : DescribeSpec({
     isolationMode = IsolationMode.InstancePerLeaf
 
-    val itemName = "캠핑덕 우드롤테이블"
-    val itemId = 778422L
-    var itemPrice: BigDecimal
-    var deliveryFee: BigDecimal
-    var orderQuantity: Int
-    var stockQuantity: Int
-
     describe("주문 시") {
         context("주문금액이 5만원 이상이라면") {
-            orderQuantity = 2
-            stockQuantity = 7
-            itemPrice = BigDecimal.valueOf(45000)
-            val item = Item(itemId, itemPrice, itemName, stockQuantity)
-            val orderData = OrderData(itemId, orderQuantity, item)
             val calculator = PriceCalculator()
+            val orderData = createOrderData(
+                itemPrice = BigDecimal.valueOf(45000),
+                itemName = "캠핑덕 우드롤테이블", stockQuantity = 7, orderQuantity = 2
+            )
             val sut = Order()
 
             val result = sut.createWith(orderData, calculator)
 
             it("주문금액에 배송료를 포함하지 않는다") {
-                result.price shouldBe itemPrice.multiply(BigDecimal(orderQuantity))
+                result.price shouldBe BigDecimal.valueOf(90000)
             }
             it("주문이 성공한다") {
                 result.isSuccess shouldBe true
             }
         }
         context("주문금액이 5만원 미만이라면") {
-            orderQuantity = 1
-            stockQuantity = 7
-            itemPrice = BigDecimal.valueOf(45000)
-            deliveryFee = BigDecimal.valueOf(2500)
-            val item = Item(itemId, itemPrice, itemName, stockQuantity)
-            val orderData = OrderData(itemId, orderQuantity, item)
             val calculator = PriceCalculator()
+            val orderData = createOrderData(
+                itemPrice = BigDecimal.valueOf(45000),
+                itemName = "캠핑덕 우드롤테이블", stockQuantity = 7, orderQuantity = 1
+            )
             val sut = Order()
 
             val result = sut.createWith(orderData, calculator)
 
             it("주문금액에 배송료를 포함한다") {
-                result.price shouldBe (itemPrice + deliveryFee)
+                result.price shouldBe BigDecimal.valueOf(47500)
             }
             it("주문이 성공한다") {
                 result.isSuccess shouldBe true
             }
         }
         context("상품의 재고 보다 많은 수량이 주문된다면") {
-            orderQuantity = 10
-            stockQuantity = 7
-            itemPrice = BigDecimal.valueOf(45000)
-            val item = Item(itemId, itemPrice, itemName, stockQuantity)
-            val orderData = OrderData(itemId, orderQuantity, item)
             val calculator = PriceCalculator()
+            val orderData = createOrderData(
+                itemPrice = BigDecimal.valueOf(45000),
+                itemName = "캠핑덕 우드롤테이블", stockQuantity = 7, orderQuantity = 10
+            )
+
             val sut = Order()
 
             val result = sut.createWith(orderData, calculator)
@@ -74,3 +64,14 @@ internal class OrderUnitTest : DescribeSpec({
         }
     }
 })
+
+const val ITEM_ID = 778422L
+private fun createOrderData(
+    itemPrice: BigDecimal,
+    itemName: String,
+    stockQuantity: Int,
+    orderQuantity: Int
+): OrderData {
+    val item = Item(ITEM_ID, itemPrice, itemName, stockQuantity)
+    return OrderData(ITEM_ID, orderQuantity, item)
+}
